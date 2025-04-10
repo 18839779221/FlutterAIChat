32edbf3614b021ef8aa2e6f2c78c79c4c355c10d
wanglun03 <wanglun03@zuoyebang.com>
2025-04-10 15:26:55 +0800

    支持以fvm形式的flutter版本

--- lib/widgets/chat_input.dart
+++ lib/widgets/chat_input.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 class ChatInput extends StatelessWidget {
+
   final TextEditingController controller;
   final Function(String) onSendMessage;
 
@@ -12,11 +13,15 @@ class ChatInput extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final bottomHomeHeight = MediaQuery.of(context).padding.bottom; // Home 栏高度
+    final keyboardHeight = MediaQuery.of(context).viewInsets.bottom; // 键盘高度
+
     return Container(
       decoration: BoxDecoration(
         border: Border(top: BorderSide(color: Colors.grey[300]!)),
       ),
       padding: const EdgeInsets.all(8.0),
+      margin: EdgeInsets.only(bottom: keyboardHeight > 0 ? 0 : bottomHomeHeight),
       child: Row(
         children: [
           Expanded(
