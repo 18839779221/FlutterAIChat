4b848a5ab1e71fbd82329c608aafc41823d3e7e4
wanglun03 <wanglun03@zuoyebang.com>
2026-04-27 21:39:23 +0800

    refactor: remove dead transcript builder paths

--- test/controllers/chat_send_coordinator_test.dart
+++ test/controllers/chat_send_coordinator_test.dart
@@ -396,7 +396,6 @@ class _FakeTurnHarness extends TurnHarness {
             eventRepository: ChatEventRepository(databaseHelper),
           ),
           turnVerifier: TurnVerifier(),
-          chatService: ChatService(llm: _NoopBaseLLM()),
           toolCallService: ToolCallService(
             toolExecutor: ToolExecutor(chatStorage: databaseHelper),
           ),
